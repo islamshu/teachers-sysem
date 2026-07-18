71ad98120415547f0a5bc23826aca8b1f1ba8a38
islamshu12 <islamshu12@gmail.com>
2026-07-18 21:37:07 +0300

    update Telegram webhook route to GET method

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -7,12 +7,14 @@ use Illuminate\Support\Facades\Log;
 
 class TelegramController extends Controller
 {
-    public function webhook(Request $request)
-    {
-        Log::info($request->all());
+   public function webhook(Request $request)
+{
+    file_put_contents(
+        storage_path('telegram.txt'),
+        json_encode($request->all(), JSON_PRETTY_PRINT) . PHP_EOL,
+        FILE_APPEND
+    );
 
-        return response()->json([
-            'ok' => true
-        ]);
-    }
+    return response()->json(['ok' => true]);
+}
 }

105c8c31481b804c4d834dafbfeb4ad1927cd321
islamshu12 <islamshu12@gmail.com>
2026-07-18 21:33:53 +0300

    update Telegram webhook route to GET method

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\TelegramController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/telegram/webhook', [TelegramController::class, 'webhook']);
+Route::post('/telegram/webhook', [TelegramController::class, 'webhook']);
